eb03edff761bab9cb763363c14a30c58611c085c
xneyder <xneyder@gmail.com>
2023-11-15 21:13:09 -0500

    Add files

--- main.tf
+++ main.tf
@@ -242,14 +242,14 @@ resource "aws_ecr_registry_scanning_configuration" "this" {
   scan_type = var.registry_scan_type
 
   dynamic "rule" {
-    for_each = var.registry_scan_rules
+    for_each = toset(var.registry_scan_rules)
 
     content {
       scan_frequency = rule.value.scan_frequency
 
       repository_filter {
         filter      = rule.value.filter
-        filter_type = try(rule.value.filter_type, "WILDCARD")
+        filter_type = rule.value.filter_type
       }
     }
   }
