07cca806289bed296f8430de70cd39a89dd1a901
xneyder <xneyder@gmail.com>
2023-11-27 12:21:51 -0500

    fix

--- main.tf
+++ main.tf
@@ -248,7 +248,7 @@ resource "aws_ecr_registry_scanning_configuration" "this" {
       scan_frequency = rule.value.scan_frequency
 
       dynamic "repository_filter" {
-        for_each = try(rule.value.repository_filters, [])
+        for_each = try(rule.value.repository_filter, [])
         content {
           filter      = repository_filter.value.filter
           filter_type = repository_filter.value.filter_type
